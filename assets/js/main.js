64bedd2245f1bd948ef17dbad24624ad6420ff90
lucas maciel <lucasmaciel63@gmail.com>
2021-03-17 09:00:09 -0300

    First

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+
+function calculoImc(){
+    const form = document.querySelector('.form');
+    
+   
+    function calculo(event){
+        event.preventDefault();
+        console.log('blabal');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+        let imc = Number(peso.value)/(Number(altura.value)**2);
+        
+        setResultado(`${imc}`);
+        if (imc < 18.5){
+            alert('Você está abaixo do peso');
+        }
+        else if(imc>=18.5 && imc<24.9){
+            alert('Seu peso está normal');
+        }
+        else if(imc>=25 && imc<29.9){
+            alert('Você tem sobrepeso');
+        }
+        else if(imc>=30 && imc<34.9){
+            alert('Obesidade grau 1');
+        }
+        else if(imc>=35 && imc<39.9){
+            alert('Obesidade grau 2');
+        }
+        else if(imc>=40){
+            alert('Obesidade nivel 3');
+        }
+    }
+    form.addEventListener('submit', calculo)
+
+    function setResultado(msg){
+        const texto = document.querySelector('.texto');
+        texto.innerHTML = '';
+        const p = document.createElement('p');
+        p.classList.add('paragrafo-resultado');
+        p.innerHTML = `IMC = ${msg}`;
+        texto.appendChild(p);
+    }
+}
+calculoImc();
